091f603e529d1748ce3a934cc3066515352f6c9a
pastorpeixoto <pastorpeixoto@hotmail.com>
2026-08-18 10:30:15 -0300

    Fix syntax for title and write functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.title{"App Manutenção de Carro"}
-st.write{"Seu aplicativo de manutenção está online!"}
 
-
+st.title("App Manutenção de Carro")
+st.write("Seu aplicativo de manutenção está online!")
