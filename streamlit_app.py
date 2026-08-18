255786f271b661694807a1beb7e807fcae8b715c
pastorpeixoto <pastorpeixoto@hotmail.com>
2026-08-18 10:23:05 -0300

    Update app message for maintenance status

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 st.title{"App Manutenção de Carro"}
-st.write{"Seu aplicativo de manutenção está online e funcionando perfeitamente!"}
+st.write{"Seu aplicativo de manutenção está online!"}
 
 
